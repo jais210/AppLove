a060a58332dba9dbb199a5f8d8a5c6541861f0ec
Jackeline Isabel Puruaya <jackelineisabel.puruaya@gmail.com>
2017-09-18 00:09:14 -0500

    Update main.js
    finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-// alert("hola");
+$('.col-4').click(function(e){
+  var img = e.target.src;
+  var modal = '<div class="modal" id="modal"><img src="'+ img + '" class="modal__img"><div class="boton" id="boton">X</div></div>';
+  $('body').append(modal);
+  $('#boton').click(function(){
+    $('#modal').remove();
+  })
+});
+
+
+$(document).keyup(function(e){
+  if (e.which==27) {
+    $('#modal').remove();
+  }
+})
